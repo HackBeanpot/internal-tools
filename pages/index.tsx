d4e8b7b5716fe2325f48273dfa027c1553f1a725
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2022-05-25 19:55:50 -0400

    preliminary draft for tools page, still need to make the page resizeable through @media

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
 import type { NextPage } from "next";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "../styles/theme";
+import { Divider } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const Home: NextPage = () => {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return <ThemeProvider theme={theme}>
+    <div className = "tool-box">
+    <h3>Tools</h3>
+    <Divider light />
+    <Typography variant="body1" component="p">
+    🛠 Internal tools + useful things for core members like…
+    </Typography>
+    <Typography variant="body1" component="ul">
+
+    <li>
+    <a href="https://tools.hackbeanpot.com/sigmaker/">An email signature generator</a> 
+     (last updated September 2019)
+      </li>
+
+
+    
+    </Typography>
+    </div>
+    
+    
+   
+
+
+   
+  </ThemeProvider>;
 };
 
 export default Home;
