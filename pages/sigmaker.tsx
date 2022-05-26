d14fa7abcaab7f798b6b3bd367bccad0b6c87c6e
jzhzhang66 <jzhzhang66@gmail.com>
2022-05-25 22:00:05 -0400

    testing out stuff

--- pages/sigmaker.tsx
+++ pages/sigmaker.tsx
@@ -1,56 +1,68 @@
 import type { NextPage } from "next";
-import { Button } from '@mui/material';
-import { Container } from '@mui/material';
+import { Button } from "@mui/material";
+import { Container } from "@mui/material";
 import { Divider } from "@mui/material";
-import { FormControl } from '@mui/material';
-import { Stack } from '@mui/material';
+import { FormControl } from "@mui/material";
+import { Stack } from "@mui/material";
 import { TextField } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import { theme } from "../styles/theme";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 const Sigmaker: NextPage = () => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
   return (
     <ThemeProvider theme={theme}>
-      <Container> 
-
-      <Typography variant="h3"> Signature Maker </Typography>
-      <Divider />
-      <Typography variant="body1"> Enter your info here! </Typography>
-      <FormControl sx={{ width: '25ch' }}> <Stack spacing={2}>
-      
-      <TextField
-          label="Full name"
-          id="filled-size-small"
-          variant="filled"
-          size="small"
-        />
-
-    
-      <TextField
-          label="Title"
-          id="filled-size-small"
-          variant="filled"
-          size="small"
-        /> 
-      <TextField
-          label="Phone"
-          id="filled-size-small"
-          variant="filled"
-          size="small"
-        /> 
-      
-        <Stack direction="row" spacing={1}>
-        <TextField
-          label="Email"
-          id="filled-size-small"
-          variant="filled"
-          size="small"
-        /> @hackbeanpot.com
-</Stack>
-    </Stack> </FormControl>  
-      <div> <Button size='medium' variant='contained'> Generate email signature! </Button> </div>
-      </Container> 
+      <Container sx={{ mt: 4 }}>
+        <Typography variant="h3"> Signature Maker </Typography>
+        <Divider />
+        <br />
+        <Typography variant="h5"> Enter your info here! </Typography>
+        <FormControl sx={{ my: 3 }}>
+          <Stack spacing={3}>
+            <TextField
+              label="Full name"
+              id="filled-size-small"
+              variant="filled"
+              size="small"
+              sx={{ width: "25ch" }}
+            />
+            <TextField
+              label="Title"
+              id="filled-size-small"
+              variant="filled"
+              size="small"
+              sx={{ width: "25ch" }}
+            />
+            <TextField
+              label="Phone"
+              id="filled-size-small"
+              variant="filled"
+              size="small"
+              sx={{ width: "25ch" }}
+            />
+            <Grid sx={{ display: matches ? "flex" : "block" }}>
+              <TextField
+                label="Email" 
+                id="filled-size-small"
+                variant="filled"
+                size="small"
+                sx={{ width: "25ch" }}
+              />
+              <Typography sx={{ mt: 2, ml: 1 }}>@hackbeanpot.com</Typography>
+            </Grid>
+          </Stack>
+        </FormControl>
+        <div>
+          <Button size="large" color="info" variant="contained">
+            Generate signature!
+          </Button>
+        </div>
+      </Container>
     </ThemeProvider>
   );
 };
