8fc9e171e56357c30f7e26d46c9ab2b71f5a724e
jzhzhang66 <jzhzhang66@gmail.com>
2022-06-07 07:54:27 -0400

    working

--- pages/emailSender.tsx
+++ pages/emailSender.tsx
@@ -5,11 +5,29 @@ import { theme } from "../styles/theme";
 
 const EmailSender: NextPage = () => {
   const [file, setFile] = useState();
+  const [array, setArray] = useState([]);
   if (typeof window !== "undefined") {
     var reader = new window.FileReader();
   }
   const handleOnChange = (e) => {
     setFile(e.target.files[0]);
+  };
+
+  const csvFileToArray = (string) => {
+    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+    console.log(csvRows);
+
+    const array = csvRows.map((i) => {
+      const values = i.split(",");
+      const obj = csvHeader.reduce((object, header, index) => {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return obj;
+    });
+
+    setArray(array);
   };
 
   const handleOnSubmit = (e) => {
@@ -18,12 +36,15 @@ const EmailSender: NextPage = () => {
     if (file) {
       reader.onload = function (event) {
         const csvOutput = event.target.result;
-        console.log(csvOutput);
+        csvFileToArray(csvOutput);
       };
 
       reader.readAsText(file);
     }
   };
+
+  const headerKeys = Object.keys(Object.assign({}, ...array));
+
   return (
     <ThemeProvider theme={theme}>
       <Container sx={{ mt: 4 }}>
@@ -46,6 +67,25 @@ const EmailSender: NextPage = () => {
           IMPORT CSV
         </button>
       </form>
+      <table>
+        <thead>
+          <tr key={"header"}>
+            {headerKeys.map((key) => (
+              <th>{key}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {array.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((val) => (
+                <td>{val}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </ThemeProvider>
   );
 };
