c9296140c51e994b4b9957e7dc2ae90d90b403d5
jzhzhang66 <jzhzhang66@gmail.com>
2022-06-18 00:16:45 -0400

    workigngit add -A

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,45 +3,39 @@ import { ThemeProvider } from "@mui/material";
 import { theme } from "../styles/theme";
 import { Divider } from "@mui/material";
 import { Typography } from "@mui/material";
-import { signIn, useSession } from "next-auth/react";
-
-
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
   if (!session) {
-    return ( 
+    return (
       <>
         Not signed in <br />
-        <button onClick={() => signIn('google', { callbackUrl: 'http://localhost:3000/api/auth/callback/google' })}>Sign in</button>
+        <button onClick={() => signIn()}>Sign in</button>
       </>
-    )
+    );
   }
-  return <ThemeProvider theme={theme}>
-    <div className = "tool-box">
-    <h3>Tools</h3>
-    <Divider light />
-    <Typography variant="body1" component="p">
-    🛠 Internal tools + useful things for core members like…
-    </Typography>
-    <Typography variant="body1" component="ul">
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="tool-box">
+        <h3>Tools</h3>
+        <Divider light />
+        <Typography variant="body1" component="p">
+          🛠 Internal tools + useful things for core members like…
+        </Typography>
+        <Typography variant="body1" component="ul">
+          <li>
+            <a href="https://tools.hackbeanpot.com/sigmaker/">
+              An email signature generator
+            </a>
+            (last updated September 2019)
+          </li>
 
-    <li>
-    <a href="https://tools.hackbeanpot.com/sigmaker/">An email signature generator</a> 
-     (last updated September 2019)
-      </li>
-
-
-    
-    </Typography>
-    </div>
-    
-    
-   
-
-
-   
-  </ThemeProvider>;
+          <button onClick={() => signOut()}>Sign out</button>
+        </Typography>
+      </div>
+    </ThemeProvider>
+  );
 };
 
-export default Home
+export default Home;
