e40098d732bd313c781dcd4d6e4c119654855017
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2022-06-19 12:20:31 -0400

    removed comments

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,10 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-// dynamic route handler
-export default NextAuth({ // a component?
-  providers: [ // an array
+export default NextAuth({
+  providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID, // fields
+      clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
       authorization: {
         params: {
@@ -16,9 +15,7 @@ export default NextAuth({ // a component?
       }
     })
   ],
-  // all requests to callback will be handled by NextAuth
-  callbacks: { // what is a callback?
-    // functions running in parallel with other functions
+  callbacks: {
     async signIn ({ account, profile }) {
       if (account.provider === 'google') {
         return profile.email_verified && profile.email.endsWith('@hackbeanpot.com')
@@ -26,14 +23,12 @@ export default NextAuth({ // a component?
       return false
     },
     async jwt ({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
       if (account) {
         token.accessToken = account.access_token
       }
       return token
     },
     async session ({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken
       return session
     }
