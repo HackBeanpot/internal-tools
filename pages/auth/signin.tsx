cfd387cdbdaf7391c643c1173c4caaf599fc5e7c
karynayen <karynayen@gmail.com>
2022-06-22 20:37:55 -0400

    messages

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -3,6 +3,7 @@ import { StyledLogoImage } from "../../pageStyles/sigmaker.styles";
 import { StyledButton } from '../../styles/common';
 import { StyledSignInPageContainer } from "../../pageStyles/signin.styles";
 import { icons } from '../../styles/icons';
+import { Typography } from "@mui/material";
 
 
 export default function SignIn({ providers }) {
@@ -15,6 +16,8 @@ export default function SignIn({ providers }) {
                       src={icons.HBP_LOGO.image}
                       alt={icons.HBP_LOGO.altText}
                     />
+            <Typography variant="body1">Welcome to our internal tools portal!!</Typography>
+            <br></br>
           <StyledButton onClick={() => signIn(provider.id)}>
             Sign in with {provider.name}
           </StyledButton>
