3a9fb10e4c9bc10295aa9156429ffd8803e73e3f
karynayen <karynayen@gmail.com>
2022-06-22 20:37:55 -0400

    messages

--- pageStyles/signin.styles.ts
+++ pageStyles/signin.styles.ts
@@ -1,10 +1,16 @@
 import { StyledPageContainer } from "../styles/common";
 import { styled } from '@mui/material/styles';
 
-const StyledSignInPageContainer = styled(StyledPageContainer)({
+const StyledSignInPageContainer = styled("div")({
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center", 
+    position: "fixed",  
+    left: 0, 
+    right: 0, 
+    top: 0, 
+    bottom: 0, 
+    marginTop: -100
   })
 export { StyledSignInPageContainer }
