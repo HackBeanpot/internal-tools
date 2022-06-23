9fe9b2eadce235f541bb944bf9ee298e56067e56
karynayen <karynayen@gmail.com>
2022-06-22 20:00:09 -0400

    trying to center

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,17 +1,25 @@
 import { getProviders, signIn } from "next-auth/react"
+import { StyledLogoImage } from "../../pageStyles/sigmaker.styles";
+import { StyledButton, StyledPageContainer } from '../../styles/common';
+import { icons } from '../../styles/icons';
+
 
 export default function SignIn({ providers }) {
   return (
-    <>
-    this is the sign in page
+    <StyledPageContainer>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button onClick={() => signIn(provider.id)}>
+           <StyledLogoImage
+                      id="preview-image-url"
+                      src={icons.HBP_LOGO.image}
+                      alt={icons.HBP_LOGO.altText}
+                    />
+          <StyledButton onClick={() => signIn(provider.id)}>
             Sign in with {provider.name}
-          </button>
+          </StyledButton>
         </div>
       ))}
-    </>
+    </StyledPageContainer>
   )
 }
 
