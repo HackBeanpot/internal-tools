c5f4977c80793a528e3590d498a01ecc97a02066
karynayen <karynayen@gmail.com>
2022-06-23 20:15:26 -0400

    fix redirect

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,23 +1,33 @@
-import { getProviders, signIn } from "next-auth/react"
+import React, { useEffect } from 'react';
+import { getProviders, signIn, useSession } from "next-auth/react"
 import { StyledLogoImage } from "../../pageStyles/sigmaker.styles";
 import { StyledButton } from '../../styles/common';
 import { StyledSignInPageContainer } from "../../pageStyles/signin.styles";
 import { icons } from '../../styles/icons';
 import { Typography } from "@mui/material";
+import { useRouter } from 'next/router';
+
 
 
 export default function SignIn({ providers }) {
+  const router = useRouter();
+  const session = useSession();
+  useEffect(() => {
+    if (session.data) {
+      router.push('/');
+    }
+  }, [router, session.data]);
   return (
     <>
       {Object.values(providers).map((provider) => (
         <StyledSignInPageContainer key={provider.name}>
-           <StyledLogoImage
-                      id="preview-image-url"
-                      src={icons.HBP_LOGO.image}
-                      alt={icons.HBP_LOGO.altText}
-                    />
-            <Typography variant="body1">Welcome to our internal tools portal!!</Typography>
-            <br></br>
+          <StyledLogoImage
+            id="preview-image-url"
+            src={icons.HBP_LOGO.image}
+            alt={icons.HBP_LOGO.altText}
+          />
+          <Typography variant="body1">Welcome to our internal tools portal!!</Typography>
+          <br></br>
           <StyledButton onClick={() => signIn(provider.id)}>
             Sign in with {provider.name}
           </StyledButton>
