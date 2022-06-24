9f8114de476584633940ba845e43120048869ea6
karynayen <karynayen@gmail.com>
2022-06-23 21:20:00 -0400

    fix providers error

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -7,8 +7,10 @@ import { icons } from '../../styles/icons'
 import { ThemeProvider, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 import { theme } from '../../styles/theme'
+import { InferGetServerSidePropsType } from 'next'
 
-export default function SignIn ({ providers }) {
+
+export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter()
   const session = useSession()
   useEffect(() => {
@@ -16,27 +18,29 @@ export default function SignIn ({ providers }) {
       router.push('/')
     }
   }, [router, session.data])
-  return (
-    <ThemeProvider theme={theme}>
-      {Object.values(providers).map((provider) => (
-        <StyledSignInPageContainer key={provider.name}>
-          <StyledLogoImage
-            id="preview-image-url"
-            src={icons.HBP_LOGO.image}
-            alt={icons.HBP_LOGO.altText}
-          />
-          <Typography variant="body1">Welcome to our internal tools portal!!</Typography>
-          <br></br>
-          <StyledButton bgColor={theme.palette.HBPNavy.main} onClick={() => signIn(provider.id)}>
-            Sign in with {provider.name}
-          </StyledButton>
-        </StyledSignInPageContainer>
-      ))}
-    </ThemeProvider>
-  )
+  if (providers != null) {
+    return (
+      <ThemeProvider theme={theme}>
+        {Object.values(providers).map((provider) => (
+          <StyledSignInPageContainer key={provider.name}>
+            <StyledLogoImage
+              id="preview-image-url"
+              src={icons.HBP_LOGO.image}
+              alt={icons.HBP_LOGO.altText}
+            />
+            <Typography variant="body1">Welcome to our internal tools portal!!</Typography>
+            <br></br>
+            <StyledButton bgColor={theme.palette.HBPNavy.main} onClick={() => signIn(provider.id)}>
+              Sign in with {provider.name}
+            </StyledButton>
+          </StyledSignInPageContainer>
+        ))}
+      </ThemeProvider>
+    )
+  }
 }
 
-export async function getServerSideProps () {
+export async function getServerSideProps() {
   const providers = await getProviders()
   return {
     props: { providers }
