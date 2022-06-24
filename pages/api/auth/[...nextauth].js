fb57f7c60fef518b6a012cd13f255255519e7ff1
jzhzhang66 <jzhzhang66@gmail.com>
2022-06-23 21:08:51 -0400

    fixed bgColor type error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,28 +16,28 @@ export default NextAuth({
     })
   ],
   callbacks: {
-    async signIn({ account, profile }) {
+    async signIn ({ account, profile }) {
       if (account.provider === 'google') {
         return profile.email_verified && profile.email.endsWith('@hackbeanpot.com')
       }
       return false
     },
-    async jwt({ token, account }) {
+    async jwt ({ token, account }) {
       if (account) {
         token.accessToken = account.access_token
       }
       return token
     },
-    async session({ session, token, user }) {
+    async session ({ session, token, user }) {
       session.accessToken = token.accessToken
       return session
     }
   },
   pages: {
-     signIn: '/auth/signin',
+    signIn: '/auth/signin',
     signOut: '/auth/signin',
     error: '/auth/signin',
-    verifyRequest: '/auth/signin',
+    verifyRequest: '/auth/signin'
   },
   secret: process.env.JWT_SECRET
 })
