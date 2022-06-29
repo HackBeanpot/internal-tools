5f9edeaf1ef351eb366ecf3a379fe02a3dadadd3
Dhivas <dhivassugumar@gmail.com>
2022-06-29 04:25:54 -0700

    Completed redirect and removed signout button.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,11 @@ import { theme } from '../styles/theme'
 import { signIn, useSession } from 'next-auth/react'
 import { TextContainer } from '../pageStyles/home.styles'
 import Layout from '../components/layout/Layout'
+import { GetServerSideProps } from 'next';
+import { getServerSideSessionOrRedirect } from '../server/getServerSideSessionOrRedirect';
 
 const Home: NextPage = () => {
   const { data: session } = useSession()
-  if (!session) {
-    return (
-      <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-      </>
-    )
-  }
   return (
     <Layout>
     <ThemeProvider theme={theme}>
@@ -49,5 +43,8 @@ const Home: NextPage = () => {
     </Layout>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = getServerSideSessionOrRedirect;
+
 
 export default Home
