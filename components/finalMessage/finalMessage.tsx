e19e6dd154e135d4b49fe00524e1fedd7d4c2dd5
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-07-13 19:18:26 -0700

    Added logic to handle edit for final message

---
+++ components/finalMessage/finalMessage.tsx
@@ -0,0 +1,67 @@
+import React, { ChangeEvent, useState } from 'react'
+
+import { Typography, Button } from '@mui/material';
+
+import { StyledFinalMessageContent, StyledTextField } from '../../pageStyles/emailSender.styles'
+
+
+type FinalMessageProps = {
+    id: string,
+    to: string,
+    subject: string,
+    content: string
+};
+
+export default function FinalMessage({ id, to, subject, content }: FinalMessageProps) {
+    const toMessage = to
+    const subjectMessage = subject
+    const idMail = id
+    const contentMessage = content
+
+
+    const [isEditing, setIsEditing] = useState(false)
+    const [messageContent, setMessageContent] = useState(content)
+
+
+    const handleEditButton = () => {
+        setIsEditing(true)
+        
+    }
+
+    const handleEditMessage = (e: any) => {
+        setMessageContent(e.target.value)
+    }
+
+
+    return (
+        <div id={idMail}>
+            <Typography variant="body1">To:{toMessage}</Typography>
+            <Typography variant="body1">Subject: {subjectMessage}</Typography>
+            <br />
+            <Typography variant="body1">Content:</Typography>
+            <br />
+            <Typography variant="body1">
+
+                {isEditing ?
+                    <StyledTextField
+                        value={messageContent}
+                        id="outlined-basic"
+                        label="Email subject"
+                        variant="outlined"
+                        onChange={handleEditMessage}>
+                    </StyledTextField>:
+                     <StyledFinalMessageContent>
+                        <Button
+                        variant="contained"
+                        onClick={}
+                        >
+
+                        </Button>
+
+                        {messageContent}
+                     </StyledFinalMessageContent>
+                }
+            </Typography>
+        </div>
+    )
+}
