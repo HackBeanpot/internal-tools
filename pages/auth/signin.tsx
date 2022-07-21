46340b45b4e12e18718cab832f4e15efeca163c8
jzhzhang66 <jzhzhang66@gmail.com>
2022-07-21 19:22:37 -0400

    added providers console log

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -53,6 +53,7 @@ export default function SignIn ({
 
 export async function getServerSideProps () {
   const providers = await getProviders()
+  console.log(providers)
   return {
     props: { providers }
   }
