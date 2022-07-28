4de7e81e8e3408f0d29e50a6401e2227877cfd26
mikeULCRM <104475280+mikeULCRM@users.noreply.github.com>
2022-07-28 11:15:31 -0400

    added DisplayMessages component

---
+++ components/displayMessages/displayMessages.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyledFinalMessagesContainer } from '../../pageStyles/emailSender.styles'
+
+type DisplayMessagesProps = {
+    displayMessages: any,
+}
+
+export default function DisplayMessages ({ displayMessages } :DisplayMessagesProps) {
+  return (
+    <StyledFinalMessagesContainer>
+        {displayMessages()}
+    </StyledFinalMessagesContainer>
+  )
+}
