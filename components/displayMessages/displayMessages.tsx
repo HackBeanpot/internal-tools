42d56212005bc2a693c901c3c472176e34d25879
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-07-28 18:21:54 -0700

    Refactored code to display final messages.

--- components/displayMessages/displayMessages.tsx
+++ components/displayMessages/displayMessages.tsx
@@ -1,16 +1,37 @@
 import React from 'react'
+import { Message } from '../../lib/types'
+import { StyledDivider, StyledErrorMessage } from '../../pageStyles/emailSender.styles'
 import { StyledFinalMessagesContainer } from './displayMessages.styles'
+import FinalMessage from '../../components/finalMessage/finalMessage'
+import { nanoid } from 'nanoid'
 
 type DisplayMessagesProps = {
-  displayMessages: any;
+  finalMessages: Message[],
+  editFinalMessages: any,
+  getErrorMessage: any
 };
 
 export default function DisplayMessages ({
-  displayMessages
+  finalMessages, editFinalMessages, getErrorMessage
 }: DisplayMessagesProps) {
   return (
     <StyledFinalMessagesContainer>
-      {displayMessages()}
-    </StyledFinalMessagesContainer>
+{finalMessages.map((msg) => (
+          <div key={nanoid()}>
+            <StyledDivider />
+            {getErrorMessage(msg.id) && (
+              <StyledErrorMessage>
+                Error: {getErrorMessage(msg.id)}
+              </StyledErrorMessage>
+            )}
+            <FinalMessage
+              id={msg.id}
+              to={msg.to}
+              subject={msg.subject}
+              parentCallback={editFinalMessages}
+              content={msg.content}
+            />
+          </div>
+))}    </StyledFinalMessagesContainer>
   )
 }
