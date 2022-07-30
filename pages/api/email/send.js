a879dcafd15fa62eba6da4c0001325519516de73
Dean Frame <dacframe@gmail.com>
2022-07-29 22:07:23 -0700

    unstyled

--- pages/api/email/send.js
+++ pages/api/email/send.js
@@ -1,6 +1,8 @@
 import { sendEmail } from './util/sendEmail'
 
 export default async function handler (req, res) {
-  const [status, message] = await sendEmail(req.body.emailData, req.body.from, req.body.date)
+  const [status, message] = await sendEmail(
+    req.body.emailData, req.body.from, req.body.date, req.body.signature
+  )
   res.status(status).json({ result: message })
 }
