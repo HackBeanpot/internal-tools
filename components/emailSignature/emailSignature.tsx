2cc0b7abb9ba3b656b5963e7bcf7e744ffcc1f35
Dean Frame <dacframe@gmail.com>
2022-07-29 23:29:22 -0700

    styled

--- components/emailSignature/emailSignature.tsx
+++ components/emailSignature/emailSignature.tsx
@@ -1,17 +1,5 @@
 import React from 'react'
 import { SignatureData } from '../../lib/types'
-import {
-  StyledContentContainer,
-  StyledLogoContainer,
-  StyledTable,
-  StyledSignatureName,
-  StyledLogoImage,
-  StyledSignatureText,
-  StyledPhoneNumber,
-  StyledLinkContainer,
-  StyledLink
-} from '../../styles/emailSignature'
-import { StyledPageContainer } from '../../styles/common'
 import { icons } from '../../styles/icons'
 import { theme } from '../../styles/theme'
 import { ThemeProvider } from '@mui/material'
@@ -20,70 +8,134 @@ interface EmailSignatureProps {
   signatureData?: SignatureData
 }
 
+// This component now uses inline styles so that it can be sent over Mailgun.
+// See styles definition below
 export default function EmailSignature ({ signatureData }: EmailSignatureProps) {
   return <>
     {signatureData &&
       (
         <ThemeProvider theme={theme}>
-          <StyledPageContainer>
-            <StyledTable cellPadding={0} cellSpacing={0}>
-              <tbody>
-                <tr>
-                  <StyledLogoContainer valign="top">
-                    <StyledLogoImage
-                      id="preview-image-url"
-                      src={icons.HBP_LOGO.image}
-                      alt={icons.HBP_LOGO.altText}
-                    />
-                  </StyledLogoContainer>
-                  <StyledContentContainer>
-                    <table cellPadding={0} cellSpacing={0}>
-                      <tbody>
-                        <tr>
-                          <StyledSignatureName colSpan={2}>
-                            {signatureData.fullName}
-                          </StyledSignatureName>
-                        </tr>
-                        <tr>
-                          <StyledSignatureText colSpan={2}>
-                            {signatureData.title}
-                          </StyledSignatureText>
-                        </tr>
-                        <tr>
-                          <StyledSignatureText colSpan={2}>
-                            <strong>HackBeanpot, Inc.</strong>
-                          </StyledSignatureText>
-                        </tr>
-                        <tr>
-                          <StyledPhoneNumber>
-                            {signatureData.phone}
-                          </StyledPhoneNumber>
-                        </tr>
-                        <tr>
-                          <StyledLinkContainer valign="top">
-                            <StyledLink
-                              href="https://hackbeanpot.com"
-                              target="_blank"
-                            >
-                              www.hackbeanpot.com
-                            </StyledLink>
-                          </StyledLinkContainer>
-                        </tr>
-                        <tr>
-                          <StyledSignatureText>
-                            <StyledLink href={`mailto:${signatureData.email}@hackbeanpot.com`}>
-                              <a>{signatureData.email}@hackbeanpot.com</a>
-                            </StyledLink>
-                          </StyledSignatureText>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </StyledContentContainer>
-                </tr>
-              </tbody>
-            </StyledTable>
-          </StyledPageContainer>
+          <table cellPadding={0} cellSpacing={0}
+            style={styles.table}>
+            <tbody>
+              <tr>
+                <td valign="top" style={styles.logoContainer}>
+                  <img
+                    id="preview-image-url"
+                    src={icons.HBP_LOGO.image}
+                    alt={icons.HBP_LOGO.altText}
+                    style={styles.logoImage}
+                  />
+                </td>
+                <td style={styles.contentContainer}>
+                  <table cellPadding={0} cellSpacing={0}>
+                    <tbody>
+                      <tr>
+                        <td colSpan={2} style={styles.signatureName}>
+                          {signatureData.fullName}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colSpan={2} style={styles.signatureText}>
+                          {signatureData.title}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td colSpan={2} style={styles.signatureText}>
+                          <strong>HackBeanpot, Inc.</strong>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style={styles.phoneNumber}>
+                          {signatureData.phone}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td valign="top" style={styles.linkContainer}>
+                          <a
+                            href='https://hackbeanpot.com'
+                            target='_blank'
+                            rel='noreferrer'
+                            style={styles.link}
+                          >
+                            www.hackbeanpot.com
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style={styles.signatureText}>
+                          <a
+                            href={`mailto:${signatureData.email}@hackbeanpot.com`}
+                            style={styles.link}
+                          >
+                            {signatureData.email}@hackbeanpot.com
+                          </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </ThemeProvider>
       )}
   </>
+}
+
+// Styles used for the signature.
+const styles = {
+  table: {
+    background: 'none',
+    borderWidth: 0,
+    border: 0,
+    margin: 0,
+    padding: 0,
+    [theme.breakpoints.up('xs')]: {
+      marginLeft: -25
+    }
+  },
+  logoContainer: {
+    paddingRight: 24,
+    border: 0
+  },
+  contentContainer: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: 12,
+    paddingRight: 0,
+    lineHeight: '21px'
+  },
+  signatureName: {
+    color: theme.palette.HBPNavy.main,
+    fontWeight: 'bold',
+    fontSize: 18,
+    fontFamily: theme.typography.fontFamily
+  },
+  logoImage: {
+    height: 132
+  },
+  signatureText: {
+    color: theme.palette.HBPNavy.main,
+    fontSize: 14,
+    fontFamily: theme.typography.fontFamily
+  },
+  phoneNumber: {
+    color: theme.palette.HBPNavy.main,
+    textDecoration: 'none',
+    fontSize: 14,
+    fontFamily: theme.typography.fontFamily
+  },
+  linkContainer: {
+    verticalAlign: 'top',
+    color: theme.palette.HBPNavy.main,
+    fontSize: 14,
+    fontFamily: theme.typography.fontFamily
+  },
+  link: {
+    color: theme.palette.Mist.main,
+    textDecoration: 'none',
+    fontWeight: 'normal',
+    fontSize: 14
+  }
 }
