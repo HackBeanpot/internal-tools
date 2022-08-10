1553a8e8227562fecc5292fbee59877dd84b1df4
Lisa Jiang <92692037+jhylisa666@users.noreply.github.com>
2022-08-09 21:59:45 -0400

    still needs styling

--- components/templateDropdown/templateDropdown.tsx
+++ components/templateDropdown/templateDropdown.tsx
@@ -3,32 +3,47 @@ import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
+import { MessageTemplate } from '../../lib/types'
+import { Typography, Stack } from '@mui/material'
 
-export default function TemplateDropdown () {
-  const [age, setAge] = React.useState('')
+type TemplateDropdownProps = {
+  templates: MessageTemplate[]
+}
+
+export default function TemplateDropdown ({ templates }: TemplateDropdownProps) {
+  const [template, setTemplate] = React.useState('')
 
   const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value)
+    setTemplate(event.target.value)
   }
 
   return (
     <div>
-      <FormControl sx={{ m: 1, minWidth: 500 }}>
-        <InputLabel id="demo-simple-select-autowidth-label">Select Template</InputLabel>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 400 }}>
+        <InputLabel id="demo-simple-select-standard-label">Select Template</InputLabel>
         <Select
-          labelId="demo-simple-select-autowidth-label"
-          id="demo-simple-select-autowidth"
-          value={age}
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={template}
           onChange={handleChange}
-          autoWidth
-          label="Age"
+          label="Select Template"
         >
           <MenuItem value="">
-            <em>None</em>
+            <em>Select Template</em>
           </MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={21}>Twenty one</MenuItem>
-          <MenuItem value={22}>Twenty one and a half</MenuItem>
+          {templates.map((item: MessageTemplate) => (
+            <MenuItem key={item.messageID}>
+              <Stack direction="row" spacing={2}>
+              <Typography variant="body1">
+                {item.title}
+                </Typography>
+                <Typography variant="body1">
+                {item.timestamp.toLocaleString()}
+                </Typography>
+
+              </Stack>
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </div>
--- pages/manageTemplates.tsx
+++ pages/manageTemplates.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import type { NextPage } from 'next'
-import { Typography, ThemeProvider, Divider, Stack, Link } from '@mui/material'
+import { Typography, ThemeProvider, Divider, Stack, Link, Button } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import Layout from '../components/layout/Layout'
-import { StyledPageContainer, SectionContainer } from '../styles/common'
+import { StyledPageContainer, SectionContainer, StyledTextArea } from '../styles/common'
 import TemplateDropdown from '../components/templateDropdown/templateDropdown'
+import { MessageTemplate } from '../lib/types'
+import { StyledTextField } from '../pageStyles/emailSender.styles'
 
 const ManageTemplates: NextPage = () => {
   const theme = useTheme()
+  const template1: MessageTemplate = {
+    messageID: 1,
+    title: 'title 1',
+    message: 'message 1',
+    timestamp: new Date('December 17, 1995 03:24:00'),
+    createdBy: 'mike'
+  }
+  const template2: MessageTemplate = {
+    messageID: 2,
+    title: 'title 2',
+    message: 'message 2',
+    timestamp: new Date('December 17, 1995 03:24:00'),
+    createdBy: 'karyna'
+  }
+
+  const template3: MessageTemplate = {
+    messageID: 3,
+    title: 'title 3',
+    message: 'message 3',
+    timestamp: new Date('October 9, 2003 03:24:00'),
+    createdBy: 'dean'
+  }
+
+  const messageArray: MessageTemplate[] = [template1, template2, template3]
   return (
         <>
             <Layout>
@@ -27,9 +53,34 @@ const ManageTemplates: NextPage = () => {
                         <Divider />
                         <br />
                         <SectionContainer>
-                            <TemplateDropdown></TemplateDropdown>
+                            <TemplateDropdown templates={messageArray} />
                         </SectionContainer>
-
+                        <SectionContainer>
+                            <Stack direction="row" spacing={1}>
+                                <Typography variant="body1">Template Name</Typography>
+                                <Typography color="red">*</Typography>
+                            </Stack>
+                            <StyledTextField
+                                id="outlined-basic"
+                                variant="outlined"
+                            // onChange={handleEmailSubject}
+                            />
+                        </SectionContainer>
+                        <SectionContainer>
+                            <Stack direction="row" spacing={1}>
+                                <Typography variant="body1">Template Message</Typography>
+                                <Typography color="red">*</Typography>
+                            </Stack>
+                            <StyledTextArea
+                                aria-label="message-text-area"
+                                placeholder="Paste in message"
+                                // onChange={(e) => setMessage(e.target.value)}
+                                minRows={5}
+                            />
+                        </SectionContainer>
+                        <Button variant="contained" component="span">
+                            Create New Message
+                        </Button>
                     </StyledPageContainer>
                 </ThemeProvider>
             </Layout>
