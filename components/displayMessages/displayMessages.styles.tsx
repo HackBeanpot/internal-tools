79642ce15dacac6bfffa7b4b34250dcc40e5153c
Dhivas-Sugumar <65351416+Dhivas-Sugumar@users.noreply.github.com>
2022-08-11 10:47:49 -0400

    Salute (#83)
    * Added salute png.
    
    * OMGGGG ADDED SALUTEEEEE!
    
    * Fixed all style imports.
    
    * moved stylederror to common styles.

--- components/displayMessages/displayMessages.styles.tsx
+++ components/displayMessages/displayMessages.styles.tsx
@@ -1,3 +1,4 @@
+import { Divider } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
 const StyledFinalMessagesContainer = styled('div')({
@@ -5,4 +6,9 @@ const StyledFinalMessagesContainer = styled('div')({
   marginBottom: 50
 })
 
-export { StyledFinalMessagesContainer }
+const StyledDivider = styled(Divider)({
+  marginTop: 50,
+  marginBottom: 50
+})
+
+export { StyledFinalMessagesContainer, StyledDivider }
