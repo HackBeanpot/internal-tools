1daa25516da384bdaa80d11769478553405308d3
mike10911 <mikeymundia@gmail.com>
2022-09-23 20:13:37 -0400

    nextConnet handler fix

--- pages/api/messageTemplates/mongo.ts
+++ pages/api/messageTemplates/mongo.ts
@@ -1,22 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import middleware from '../../../lib/mongodb'
 import nextConnect from 'next-connect'
-
+import middleware from '../../../lib/mongodb'
 const handler = nextConnect<NextApiRequest, NextApiResponse>()
 handler.use(middleware)
 handler.get(async (req, res) => {
   const doc = await req.db.collection('templates').findOne()
+
+  // error checking
+  // if doc is null
+  // if doc is empty
+  // return good status messages in these cases
   res.json(doc)
 })
-
 export default handler
-
-// export async function getStaticProps () {
-//   const res = await fetch('http://localhost:3000/pages/api/messageTemplates/[id].ts')
-//   const json = await res.json()
-//   return {
-//     props: {
-//       data: json
-//     }
-//   }
-// }
