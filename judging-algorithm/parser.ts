c802b896e05bc00f24d9c3cbef68d1b10986ec17
Alyssa Mui <alyssamui23@gmail.com>
2023-05-21 22:16:26 -0400

    fix eslint

--- judging-algorithm/parser.ts
+++ judging-algorithm/parser.ts
@@ -1,36 +1,36 @@
-import * as fs from "fs";
-import * as path from "path";
-import { parse } from "csv-parse/sync";
-import { HackerTeam, Judge, Room } from "./types";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import * as fs from 'fs'
+import * as path from 'path'
+import { parse } from 'csv-parse/sync'
+import { HackerTeam, Judge, Room } from './types'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-function parseCsv<T>(csvFilePath: string, headers: string[] | boolean): T[] {
-  const csvFileAbsolutePath = path.resolve(__dirname, csvFilePath);
+function parseCsv<T> (csvFilePath: string, headers: string[] | boolean): T[] {
+  const csvFileAbsolutePath = path.resolve(__dirname, csvFilePath)
 
   const fileContent = fs.readFileSync(csvFileAbsolutePath, {
-    encoding: "utf-8",
-  });
+    encoding: 'utf-8'
+  })
   const options = {
-    delimiter: ",",
-    columns: headers,
-  };
-  return parse(fileContent, options);
+    delimiter: ',',
+    columns: headers
+  }
+  return parse(fileContent, options)
 }
 
-export function parseJudgeCSV(judgeCsvFilePath: string): Judge[] {
-  return parseCsv<Judge>(judgeCsvFilePath, true);
+export function parseJudgeCSV (judgeCsvFilePath: string): Judge[] {
+  return parseCsv<Judge>(judgeCsvFilePath, true)
 }
 
-export function parseHackerTeamCSV(
+export function parseHackerTeamCSV (
   hackerTeamCsvFilePath: string
 ): HackerTeam[] {
-  return parseCsv<HackerTeam>(hackerTeamCsvFilePath, true);
+  return parseCsv<HackerTeam>(hackerTeamCsvFilePath, true)
 }
 
-export function parseRoomsCSV(roomCsvFilePath: string): Room[] {
-  return parseCsv<Room>(roomCsvFilePath, true);
+export function parseRoomsCSV (roomCsvFilePath: string): Room[] {
+  return parseCsv<Room>(roomCsvFilePath, true)
 }
