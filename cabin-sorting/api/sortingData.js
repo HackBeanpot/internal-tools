184a06b2f29b17ff2f8efa2e809ee1ebfb25783c
kzeng24 <kathyzeng2002@gmail.com>
2023-06-06 22:23:28 -0400

    transfered previous branch's commits to this branch

--- cabin-sorting/api/sortingData.js
+++ cabin-sorting/api/sortingData.js
@@ -1,96 +1,96 @@
-import path from 'path'
-import fs from 'fs'
-import { client, connectToDatabase } from './mongodb'
+import path from "path";
+import fs from "fs";
+import { client, connectToDatabase } from "./mongodb";
 
-let validatedHackers
-const validHackers = []
+let validatedHackers;
+const validHackers = [];
 
 // Get all valid hacker data from the applicant_data database and convert to json contents
-async function grabFromDatabase () {
-  const applicantData = await connectToDatabase('applicant_data')
+async function grabFromDatabase() {
+  const applicantData = await connectToDatabase("applicant_data");
 
   try {
     // Query for hackers whose application status is submitted and who has a
     // 'Post Acceptance Response' section
     const query = {
-      applicationStatus: 'Submitted',
+      applicationStatus: "Submitted",
       postAcceptanceResponses: { $exists: true },
       isAdmin: false,
-      rsvpStatus: 'Confirmed',
-      'postAcceptanceResponses.swag': { $exists: false },
-      'postAcceptanceResponses.club': { $exists: true }
-    }
+      rsvpStatus: "Confirmed",
+      "postAcceptanceResponses.swag": { $exists: false },
+      "postAcceptanceResponses.club": { $exists: true },
+    };
 
     // Find all valid hackers that have answers to the cabin questions
     const hackerDataCursor = applicantData
       .find(query)
-      .project({ email: 1, postAcceptanceResponses: 1 })
+      .project({ email: 1, postAcceptanceResponses: 1 });
 
     // create list of hackers with their information/questions from the cursor
-    const hackerDocuments = await hackerDataCursor.toArray()
+    const hackerDocuments = await hackerDataCursor.toArray();
 
     // parse and format each hacker in the list of hackers
-    hackerDocuments.map((item) => parseAndFormatHacker(item))
+    hackerDocuments.map((item) => parseAndFormatHacker(item));
 
     // get final filtered list of valid hackers
-    validatedHackers = validateHackers(validHackers)
+    validatedHackers = validateHackers(validHackers);
 
     // convert hacker list to json and output data to fromMongoDB.json
-    const hackerJson = JSON.stringify(validatedHackers)
+    const hackerJson = JSON.stringify(validatedHackers);
     const pathToWrite = path.resolve(
       __dirname,
-      '../',
-      'data',
-      'json_outputs',
-      'fromMongoDB.json'
-    )
-    fs.writeFileSync(pathToWrite, hackerJson)
+      "../",
+      "data",
+      "json_outputs",
+      "fromMongoDB.json"
+    );
+    fs.writeFileSync(pathToWrite, hackerJson);
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close()
+    await client.close();
   }
-  return validatedHackers
+  return validatedHackers;
 }
 
 // used for parsing hacker data into a uniform, correct, format
-function parseAndFormatHacker (item) {
+function parseAndFormatHacker(item) {
   // We are taking out the postAcceptanceResponses fields and putting them on
   // same level as other fields (for easier access)
-  const postAcceptanceResponses = item.postAcceptanceResponses
-  delete item.postAcceptanceResponses
-  item = { ...item, ...postAcceptanceResponses }
+  const postAcceptanceResponses = item.postAcceptanceResponses;
+  delete item.postAcceptanceResponses;
+  item = { ...item, ...postAcceptanceResponses };
 
   // duplicate postAcceptanceResponse fields and rename fields to question<> : value
   // so we can map these question answers to cabin answers
   // (for cabin sorting in hackerSortingAlgo.ts)
-  const attributes = Object.keys(item)
+  const attributes = Object.keys(item);
   for (let i = 0; i <= 9; i++) {
-    const j = attributes.length - 9 + i
-    const attributevalue = item[attributes[j]]
+    const j = attributes.length - 9 + i;
+    const attributevalue = item[attributes[j]];
     // delete item[attributes[j]]
-    item[`question${i}`] = attributevalue
+    item[`question${i}`] = attributevalue;
   }
   // adding all hackers to list
-  validHackers.push(item)
+  validHackers.push(item);
 }
 
 // returns valid hacker list and handles errors if data doesn't exist or is empty
-function validateHackers (hackers) {
+function validateHackers(hackers) {
   if (hackers === null) {
     console.error(
-      'Oopsie daisy, something went wrong when querying for valid hackers!'
-    )
-    return []
+      "Oopsie daisy, something went wrong when querying for valid hackers!"
+    );
+    return [];
   } else if (hackers.length === 0) {
-    console.warn('Oopsie daisy, no such hackers were found!')
+    console.warn("Oopsie daisy, no such hackers were found!");
   }
 
   // ensures that hackers have non-empty email field
   // !! checks if value is null or empty string
-  hackers.filter((hacker) => !!hacker.email)
-  return hackers
+  hackers.filter((hacker) => !!hacker.email);
+  return hackers;
 }
 
-grabFromDatabase()
+grabFromDatabase();
 
-export default grabFromDatabase
+export default grabFromDatabase;
--- cabin-sorting/hackerSortingAlgo.ts
+++ cabin-sorting/hackerSortingAlgo.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import { parse } from 'csv-parse/sync'
+import grabFromDatabase from './api/sortingData.js'
 
 let hackerList: any[]
 let answerList: any[]
@@ -9,8 +10,13 @@ let CABIN_SIZE: number
 let QUESTIONS_SIZE: number
 
 // Go to the given csv filepath and parse its contents into an array
-function loadCSV (filepath: string, headers: boolean): any[] {
-  const csvFileAbsolutePath = path.resolve(__dirname, 'data', 'csv_inputs', filepath)
+function loadCSV (filepath: string, headers: boolean, delimter: string): any[] {
+  const csvFileAbsolutePath = path.resolve(
+    __dirname,
+    'data',
+    'csv_inputs',
+    filepath
+  )
 
   // error handling in case file is missing
   let fileContent
@@ -24,7 +30,7 @@ function loadCSV (filepath: string, headers: boolean): any[] {
   }
 
   const options = {
-    delimiter: ',',
+    delimiter: delimter,
     columns: headers
   }
   return parse(fileContent, options)
@@ -48,26 +54,37 @@ function matchAnswers () {
     // find backup cabin for hacker (in case the first choice fills up)
     const counterCopy = cabinScore.slice()
     counterCopy[maxIndex] = -1
-    hacker.secondAssignedCabin = cabinOptions[counterCopy.indexOf(Math.max(...counterCopy))]
+    hacker.secondAssignedCabin =
+      cabinOptions[counterCopy.indexOf(Math.max(...counterCopy))]
 
     // show their score for each cabin (for testing purposes)
     console.log(`${hacker.email}'s cabin counter: ${cabinScore}`)
   })
 }
-
+// test
 // Increment the given hacker's given cabinScore each time their answer
 // matches the Cabin's answer
 function hydrateCabinScore (hacker: any, cabinScore: number[]) {
-  answerList.forEach((cabin: any, cabinIndex: number) => {
-    for (let questionIndex = 0; questionIndex < QUESTIONS_SIZE; questionIndex++) {
+  let flag = false
+  for (let questionIndex = 0; questionIndex < QUESTIONS_SIZE; questionIndex++) {
+    flag = false
+    answerList.forEach((cabin: any, cabinIndex: number) => {
       if (
-        cabin['question' + questionIndex.toString()] ===
-        hacker['question' + questionIndex.toString()]
+        cabin['question' + questionIndex].toLowerCase().trim() ===
+        hacker['question' + questionIndex].toLowerCase().trim()
       ) {
         cabinScore[cabinIndex]++
+        flag = true
       }
+    })
+    if (!flag) {
+      console.log('THIS PERSON IS WRONG FOR SOME REASON?!?!?!')
+      console.log('\n' + hacker.email)
+      console.log(hacker['question' + questionIndex])
+      console.log(questionIndex)
     }
-  })
+  }
+  return 0
 }
 
 // Print to the console each hacker's information including their top
@@ -75,7 +92,7 @@ function hydrateCabinScore (hacker: any, cabinScore: number[]) {
 function printMembers () {
   hackerList.forEach((member) => {
     console.log(
-      `ID: ${member.id}
+      `ID: ${member.id || member._id}
       | EMAIL: ${member.email}
       | assignedCabin: ${member.assignedCabin}
       | secondAssignedCabin: ${member.secondAssignedCabin}`
@@ -86,24 +103,34 @@ function printMembers () {
 // Output the hacker data in JSON format to sortedHackers.json
 function writeDataToFile () {
   const hackerTables = JSON.stringify(hackerList)
-  const pathToWrite = path.resolve(__dirname, 'data', 'json_outputs', 'sortedHackers.json')
+  const pathToWrite = path.resolve(
+    __dirname,
+    'data',
+    'json_outputs',
+    'sortedHackers.json'
+  )
   fs.writeFileSync(pathToWrite, hackerTables)
 }
 
 // Assigns two cabins to each Hacker.
 // assignedCabin = the cabin a Hacker is best suited to
 // secondAssignedCabin = their next best cabin option
-function hackerSortingAlgo () {
+async function hackerSortingAlgo () {
   // variable values first declared globally within the file and initialized on runtime
-  hackerList = loadCSV('hackerData.csv', true)
-  answerList = loadCSV('answer.csv', true)
-  cabinList = loadCSV('cabinTypes.csv', false)[0]
+  // hackerList = loadCSV('hackerData.csv', true)
+  hackerList = await grabFromDatabase()
+  answerList = loadCSV('answer.csv', true, '|')
+  cabinList = loadCSV('cabinTypes.csv', false, ',')[0]
 
   CABIN_SIZE = Object.keys(cabinList).length
   QUESTIONS_SIZE = Object.keys(answerList[0]).length
 
   // ensuring the CSV files exists before continuing
-  if (hackerList.length === 0 || answerList.length === 0 || cabinList.length === 0) {
+  if (
+    hackerList.length === 0 ||
+    answerList.length === 0 ||
+    cabinList.length === 0
+  ) {
     console.log(
       'Please add the respective csv file(s) to the folder to run the sorting algorithm'
     )
