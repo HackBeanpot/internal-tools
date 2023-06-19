1be7319b2924f07820ed1e51bc33cfe70597809b
mike10911 <mikeymundia@gmail.com>
2023-06-19 17:35:04 -0400

    fix

--- ui/pages/cabinSorting.tsx
+++ ui/pages/cabinSorting.tsx
@@ -8,7 +8,7 @@ import {
 import { theme } from "../styles/theme";
 import { StyledButton, StyledPageContainer } from "../styles/common";
 import CSVCabinTable from "../components/csvTable/CSVCabinTable";
-
+//hi
 export default function CabinSorting() {
     return (
       <Layout>
