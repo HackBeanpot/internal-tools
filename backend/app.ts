178dacaa028c834102268691d88037b2a5d2835e
mike10911 <mikeymundia@gmail.com>
2023-06-19 16:55:35 -0400

    init server

---
+++ backend/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    })
+)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`);
+    }
+);
