3a9185b03a32559ba763ef2f2ab3c585752e8743
mike10911 <mikeymundia@gmail.com>
2023-06-23 13:50:09 -0400

    updated eslint and ticket changes

--- backend/app.ts
+++ backend/app.ts
@@ -6,15 +6,14 @@ const app = express();
 app.use(express.json());
 
 app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        credentials: true,
-    })
-)
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running in http://localhost:${PORT}`);
-    }
-);
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
