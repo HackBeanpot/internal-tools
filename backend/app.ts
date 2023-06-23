ca894dfe883ce1d0f8de362120a8cb1ee1e95113
Mike <103906123+mike10911@users.noreply.github.com>
2023-06-23 15:04:05 -0400

    Init Backend Server  (#129)
    * init server
    
    * updated eslint and ticket changes
    
    * updated port
    
    * vercel config maybe?
    
    * ts-node

---
+++ backend/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:4000',
+    credentials: true
+  })
+);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
