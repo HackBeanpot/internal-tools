315fb57e365b1dc60aced8ab39d86f739922a22a
aaryan1203 <aaryanjain1203@gmail.com>
2023-06-24 17:27:39 -0700

    trying to connect the database

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv/config'
+import mongoose from "mongoose"
+import judgesRoutes from "/judging-algorithm/data/controllers/judges-controller"
+const mongoString: string = "mongodb+srv://dbadmin:ijTyfvOMOVOirZXR@hackbeanpotcluster.unazpk3.mongodb.net"
+console.log(mongoString)
+mongoose.connect(mongoString);
+const database = mongoose.connection
 
 const app = express();
+
+app.use(judgesRoutes)
 
 app.use(express.json());
 
@@ -14,6 +23,16 @@ app.use(
 
 const PORT = process.env.PORT || 4000;
 
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
 });
+
+
