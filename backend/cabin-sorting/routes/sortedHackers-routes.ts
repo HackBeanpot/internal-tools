1c46c735e225b74d83caa0707e5b6d95056f3b49
NelsonDong41 <nelsondong41@gmail.com>
2023-07-11 00:02:00 -0400

    removed comments

--- backend/cabin-sorting/routes/sortedHackers-routes.ts
+++ backend/cabin-sorting/routes/sortedHackers-routes.ts
@@ -6,10 +6,4 @@ const router = express.Router();
 router.get('/sortedHackers', controller.getSortedHackers);
 router.post('/sortedHackers', controller.createSortedHacker);
 
-
-// router.get('/judges/:id', controller.getJudgeById);
-// router.get('/judges/:name', controller.getJudgeByName)
-// router.put('/sortedHackers/:id', () => {});
-// router.delete('/sortedHackers/:id',  () => {});
-
 export default router;
--- backend/cabin-sorting/schemas/sortedHackers-schema.ts
+++ backend/cabin-sorting/schemas/sortedHackers-schema.ts
@@ -13,55 +13,6 @@ export enum DecisionStatusOptions {
   "Admitted",
   "Waitlisted",
 }
-// export interface ApplicationResponses   {
-//   firstName : string,
-//   preferredName: string,
-//   lastName: string,
-//   pronouns: string,
-//   gender: string,
-//   unlistedGender: string,
-//   races: string[],
-//   unlistedRace: string,
-//   school: string,
-//   unlistedSchool: string,
-//   education: string,
-//   yearOfEducation: string,
-//   majors: string,
-//   minors: string,
-//   resumeLink: string,
-//   shirtSize: string,
-//   accomodations: string,
-//   hackathonsAttended: string,
-//   csClassesTaken: string,
-//   mobileAppDevelopmentFamiliarity: string,
-//   webDevelopmentFamiliarity: string,
-//   uiUxFamiliarity: string,
-//   backendFamiliarity: string,
-//   frontendFamiliarity: string,
-//   dataScienceFamiliarity: string,
-//   cybersecurityFamiliarity: string,
-//   mobileAppDevelopmentInterestLevel: string,
-//   webDevelopmentInterestLevel: string,
-//   uiUxInterestLevel: string,
-//   backendInterestLevel: string,
-//   frontendInterestLevel: string,
-//   dataScienceInterestLevel: string,
-//   cybersecurityInterestLevel: string,
-//   interestedWorkshops: string[]
-//   unlistedWorkshops: string,
-//   prevHackathonFeedback: string,
-//   hackBeanGoals: string,
-//   tedTalkTopic: string,
-//   referrers: string[]
-//   unListedReferrer: string,
-//   premadeTeam: string,
-//   interestedInTeamFormation: string,
-//   questionsToAdd: string,
-//   commentsQuestionsSuggestions: string,
-//   howCanCoreTeamHelp: string,
-//   lgbtq: string
-// }
-
 
 export const sortedHackersSchema = new mongoose.Schema(
   {
