b279b9f19e5ce59315bf96bbc01b3c05c29ac329
Alyssa Mui <alyssamui23@gmail.com>
2023-07-30 21:25:41 -0400

    remove extra comments

--- backend/judging-algorithm/output-functions.ts
+++ backend/judging-algorithm/output-functions.ts
@@ -1,7 +1,4 @@
-import fs from 'fs';
-import { HackerOutput, Judge, JudgeOutputLiveSite } from './types.js';
 import { deleteAndPostData } from './parser-functions.js';
-import { parseCsv } from './parser.js';
 import hackerData from './data/json_outputs/hackerResults.json' assert { type: "json" }
 import judgeData from './data/json_outputs/judgeResults.json' assert { type: "json" }
 
@@ -9,21 +6,10 @@ const hackerJsonPath = './data/json_outputs/hackerResults.json';
 const judgeJsonPath = './data/json_outputs/judgeResults.json';
 
 export async function readJsonAndCreateInMongo() {
-  // Read hacker JSON
-  // const hackerData: HackerOutput[] = JSON.parse(hackerJson);
-
-  // Read judge JSON
-  // const judgeJson = fs.readFileSync(judgeJsonPath, { encoding: 'utf-8' });
-  // const judgeData = parseCsv<JudgeOutputLiveSite[]>(judgeJsonPath, false)
-  // const judgeData: JudgeOutputLiveSite[] = JSON.parse(judgeJson);
-
-  deleteAndPostData('hackerTable', hackerData)
-  deleteAndPostData('judgeTable', judgeData)
   // Create hacker entries in MongoDB
-
+  deleteAndPostData('hackerTable', hackerData)
   // Create judge entries in MongoDB
-
-
+  deleteAndPostData('judgeTable', judgeData)
   console.log('Data successfully inserted into MongoDB');
 }
 
